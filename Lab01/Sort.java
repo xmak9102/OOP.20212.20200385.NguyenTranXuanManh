7b4c66a0022ec05e95b442760936a7f0033f9940
xuanmanh <90127093+xmak9102@users.noreply.github.com>
2022-04-13 10:10:48 +0700

    Update Sort.java

--- Lab01/Sort.java
+++ Lab01/Sort.java
@@ -4,22 +4,20 @@ import java.util.Arrays;
 public class Sort {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Length of arr: ");
-        int n = input.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Length of array: ");
+        int n = scanner.nextInt();
         int[] arr = new int[n];
-        int t = 0;
+        int m = 0;
         for (int i = 0; i < n; i++) {
             System.out.printf("%oth element: ", i + 1);
-            arr[i] = input.nextInt();
-            t += arr[i];
+            arr[i] = scanner.nextInt();
+            m += arr[i];
         }
         Arrays.sort(arr);
-        System.out.printf("Sorted arr[] : %s\n", 
-                           Arrays.toString(arr));
-        System.out.printf("Mean value of arr : %f", 
-                          1f*t/n);
-        input.close();
+        System.out.printf("Sorted array: %s\n", Arrays.toString(arr));
+        System.out.printf("Mean value: %.2f", 1f*m/n);
+        scanner.close();
 	System.exit(0);
     }
 }
