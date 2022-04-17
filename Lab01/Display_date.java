443c4341db66ed011e1ac49de049e7df1fc01338
xuanmanh <90127093+xmak9102@users.noreply.github.com>
2022-04-17 16:40:02 +0700

    Update Display_date.java

--- Lab01/Display_date.java
+++ Lab01/Display_date.java
@@ -17,19 +17,19 @@ public class Display_date {
         if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
     		d = 31;
     	}
-	    else if(m == 4 || m == 6 || m == 9 || m == 11) {
-		    d = 30;	  
-	    }
-	    else if(m == 2) {
-		    if(y%400 == 0 && y%100 == 0) {
-			    d = 29;
+	else if(m == 4 || m == 6 || m == 9 || m == 11) {
+		d = 30;	  
+	}
+	else if(m == 2) {
+		if(y%400 == 0 && y%100 == 0) {
+			d = 29;
 		    }
-		    else if(y%4 == 0 && y%100 != 0) {
-			    d = 29;
+		else if(y%4 == 0 && y%100 != 0) {
+			d = 29;
 		    }
-		    else {
-			    d = 28;
-		   }
+		else {
+			d = 28;
+		    }
 		}
 	    System.out.printf("Number of days in %d/%d is: %d", m, y, d);
 	    
